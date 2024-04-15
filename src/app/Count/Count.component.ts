e8804650eefd086f8232c0df5dbfb0e3d0de22f5
anasmygitrepo <ksmohmmedanas45@gmail.com>
2024-04-15 08:23:04 +0400

    statemangement

---
+++ src/app/Count/Count.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone:true,
+  selector: 'app-Count',
+  templateUrl: './Count.component.html',
+  styleUrls: ['./Count.component.css']
+})
+export class CountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CountComponent } from './Count/Count.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CountComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
